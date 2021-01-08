b77276b4e3ce71d910b31bdb8487f67ec8ab02a4
Etienne Bourganel <etienne.bourganel.com>
2021-01-08 18:06:23 +0100

    Add basic random guesses generation for machine as codebreaker

--- game.rb
+++ game.rb
@@ -2,6 +2,7 @@
 
 require_relative 'display'
 require_relative 'analyze'
+require_relative 'player'
 require 'pry'
 
 # Contains flow of a game and game operations
@@ -11,31 +12,37 @@ class Game
   attr_reader :secret_code, :round, :guess
   COLORS = [1, 2, 3, 4, 5, 6].freeze # The "colors" to play with
   def initialize
-    make_role_choice
+    set_human_name
+  end
+
+  # Ask for human player name and calls choice making
+  def set_human_name
+    puts 'Hello, what is your name?'
+    human_name = gets.chomp.to_s
+    make_role_choice(human_name)
   end
 
   # Prompt human player for name and tole, then triggers players creations
-  def make_role_choice
-    puts 'Hello, what is your name?'
-    human_name = gets.chomp.to_s
-    puts "#{human_name} Please, enter 1 to play as the codebreaker or 2 to play as the codemaker."
-    choice = gets.chomp.to_i
-    if choice == (1 || 2)
+  def make_role_choice(human_name)
+    puts "Hi there #{human_name}! Enter b to play as the codeBreaker or m to play as the codeMaker."
+    choice = gets.chomp.to_s
+    if %w[b m].include?(choice)
       create_players(choice, human_name)
-    else make_role_choice
+    else make_role_choice(human_name)
     end
   end
 
   # Start the game flow corresponding to the chosen roles
   def start_correct_game_flow
-    return unless @human.role == 'codebreaker'
-
-    start_game_flow_human_codebreaker
+    if @human.role == 'codebreaker'
+      game_flow_human_codebreaker
+    else game_flow_human_codemaker
+    end
   end
 
   # Create both players
   def create_players(choice, human_name)
-    if choice == 1
+    if choice == 'b'
       @human = Human.new('codebreaker', human_name)
       @machine = Machine.new('codemaker')
     else
@@ -51,7 +58,7 @@ class Game
 
   # Condition to keep the codebreaker to continue guessing
   def continue?
-    @round <= 10 && codebreaker_wins? == false
+    @round <= 10 && @guess != @secret_code
   end
 
   # Boolean to check if codebreaker guessed right
@@ -91,24 +98,35 @@ class Game
     @round += 1
   end
 
-  # Flow for one game with the human player as the codebreaker
-  def start_game_flow_human_codebreaker
-    round_start
-    while continue?
-      one_round
-      add_one_round
-    end
+  # Verify if any player is winning and trigger appropriate logic
+  def check_and_manage_winner
     if codebreaker_wins?
       manage_codebreaker_winning_guess
     else manage_codemaker_wins
     end
+  end
+
+  # Flow for one game with the human player as the codebreaker
+  def game_flow_human_codebreaker
+    @secret_code = define_secret_code
+    game_start_human_codebreaker
+    while continue?
+      one_round_human_codebreaker
+      add_one_round
+    end
+    end_of_game_logic
+  end
+
+  # Check if there is a winner and if player wants to play a new round
+  def end_of_game_logic
+    check_and_manage_winner
     show_winner_and_scores
     new_round?
   end
 
   # Ask player for a new round
   def new_round?
-    puts 'Want to play another round? Press Y for yes or N for no'
+    puts 'Want to play another round? Press y for yes or n for no'
     input = gets.chomp.to_s
     if input == 'y'
       sleep 1
@@ -120,8 +138,8 @@ class Game
     end
   end
 
-  # Flow for a game until one player wins
-  def one_round
+  # One round where human player plays as the codebreaker
+  def one_round_human_codebreaker
     show_round(@round)
     obtain_human_input
     codebreaker_guess
@@ -131,13 +149,55 @@ class Game
     print_colorized_array(@feedback)
   end
 
-  # First instructions at the beginning of a game and reset of secret code and round number
-  def round_start
-    @secret_code = define_secret_code
+  # Game flow where human player plays as the codemaker
+  def game_flow_human_codemaker
+    game_start_human_codemaker
+    while continue?
+      one_round_human_codemaker
+      add_one_round
+    end
+    end_of_game_logic
+  end
+
+  # One round where human player plays as the codemaker
+  def one_round_human_codemaker
+    show_round(@round)
+    puts 'The machine is thinking...'
+    sleep 1
+    @guess = define_secret_code
+    print_colorized_array(@guess)
+  end
+
+  # What to do when a game starts with human as the codemaker
+  def game_start_human_codemaker
+    reset_instance_variables_values
+    set_secret_code
+    puts 'The secret code is: '
+    print_colorized_array(@secret_code)
+  end
+
+  # Human codemaker to create a secret code + verification for valid code
+  def set_secret_code
+    puts 'Enter a 4-digit code, each digit from 1 to 6.'
+    input = gets.chomp
+    @secret_code = input.split('').map(&:to_i)
+    return unless wrong_input(@secret_code) == true
+
+    input_error_messages(@secret_code)
+    set_secret_code
+  end
+
+  # Reset values for instance variables
+  def reset_instance_variables_values
     @guess = []
     @feedback = []
     @round = 1
     @winner = ''
+  end
+
+  # What to do when a game start with human as the codebreaker
+  def game_start_human_codebreaker
+    reset_instance_variables_values
     start_game_welcome_human(@human.name)
     choices
     print_colorized_array(COLORS)
@@ -147,19 +207,24 @@ class Game
   def obtain_human_input
     input = gets.chomp
     @guess = input.split('').map(&:to_i)
-    return unless wrong_input == true
+    return unless wrong_input(@guess) == true
 
-    if correct_color_input?(@guess, COLORS) == false
-      error_wrong_input_values
-    elsif correct_input_size?(@guess, 4) == false
-      error_wrong_input_size
-    end
+    input_error_messages(@guess)
     obtain_human_input
   end
 
+  # What error message to show
+  def input_error_messages(input_ary)
+    if correct_color_input?(input_ary, COLORS) == false
+      error_wrong_input_values
+    elsif correct_input_size?(input_ary, 4) == false
+      error_wrong_input_size
+    end
+  end
+
   # Group conditions for inadequate input
-  def wrong_input
-    correct_input_size?(@guess, 4) == false || correct_color_input?(@guess, COLORS) == false
+  def wrong_input(input_ary)
+    correct_input_size?(input_ary, 4) == false || correct_color_input?(input_ary, COLORS) == false
   end
 
   # Return true if an array only contains certain values
